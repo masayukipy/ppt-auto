5d2cf99f9977246566f64ab6648db724e0b90a67
masayukipy <sg.pythondev@gmail.com>
2023-03-31 23:55:28 +0100

    tkinter

--- build.py
+++ build.py
@@ -1,6 +1,10 @@
+import os
+import threading
 import tkinter as tk
 
 import pandas
+
+import app
 
 root = tk.Tk()
 
@@ -9,6 +13,7 @@ canvas1.pack()
 
 
 def hello():
+
     label1 = tk.Label(
         root, text="utils.Utils.a", fg="blue", font=("helvetica", 12, "bold")
     )
@@ -17,8 +22,22 @@ def hello():
     canvas1.create_window(150, 200, window=label1)
 
 
-button1 = tk.Button(text="Click Me", command=hello, bg="brown", fg="white")
+def run_all():
+    # os.popen("'.venv/Scripts/python.exe' 'app.py'")
+    all_thread = threading.Thread(target=app.all)
+    hello_thread = threading.Thread(target=hello)
+    hello_thread.start()
+    all_thread.start()
+    hello_thread.join()
+    all_thread.join()
+    return
+
+
+button1 = tk.Button(text="Run All", command=run_all, bg="brown", fg="white")
 canvas1.create_window(150, 150, window=button1)
+
+entry1 = tk.Entry(root)
+canvas1.create_window(200, 140, window=entry1)
 
 
 root.mainloop()
